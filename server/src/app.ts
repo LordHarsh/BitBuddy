81bf15bff65d33c3a573858c8baaec09faf1d703
Divyanshu Yadav <dd8353@srmist.edu.in>
2023-05-23 03:18:11 +0530

    :rocket: Server Completed

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import config from "config";
 import bodyParser from "body-parser";
 import cors from "cors";
+import routes from "./routes";
 import db from "./db";
 
 const app = express();
@@ -20,4 +21,5 @@ app.use(bodyParser.json());
 app.listen(port, "0.0.0.0", () => {
   console.log(`Application listening at http://localhost:${port}`);
   db();
+  routes(app);
 });
