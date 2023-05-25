c0825e878f2b275b1a8159234ae6d4a7812115c1
Divyanshu Yadav <dd8353@srmist.edu.in>
2023-05-25 22:48:12 +0530

    :art: Refactoring

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,10 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  // const [path, setPath] = useState("");
   const router = window.location.pathname;
-  // if (router === "/history") setPath("HOME");
-  // else setPath("HISTORY");
+
   return (
     <div className="text-white flex justify-end items-center pt-16 px-10">
       <Link to={router === "/" ? "/history" : "/"} className="absolute">
