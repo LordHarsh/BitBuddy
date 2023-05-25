cf368476aa734eede87c7a085f516fe64e499ca6
Divyanshu Yadav <dd8353@srmist.edu.in>
2023-05-25 19:07:13 +0530

    🐍 Path rectified

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,13 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  // const [path, setPath] = useState("");
   const router = window.location.pathname;
-  // if (router === "/history") setPath("HOME");
-  // else setPath("HISTORY");
+
   return (
-    <div className="text-white flex justify-end items-center pt-10 px-10">
-      <Link to={router === "/" ? "/history" : "/"}>
+    <div className="text-white flex justify-end items-center pt-16 px-10">
+      <Link to={router === "/" ? "/history" : "/"} className="absolute">
         <div className="text-2xl font-bold cursor-pointer">
           {router === "/" ? "HISTORY" : "HOME"}
         </div>
