10b522e21f6eabeeec2b016f494aeb1902474574
Divyanshu Yadav <dd8353@srmist.edu.in>
2023-05-25 18:54:44 +0530

    :sparkles: History Added

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,17 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  // const [path, setPath] = useState("");
+  const router = window.location.pathname;
+  // if (router === "/history") setPath("HOME");
+  // else setPath("HISTORY");
   return (
-    <div className="text-white flex justify-between items-center pt-10 px-10">
-      <div className="">
-        <div className=" items-center flex">
-          <Link to="/" className="absolute cursor-pointer">
-            <img className="" src="/icon.png" width={80} alt="" />
-          </Link>
+    <div className="text-white flex justify-end items-center pt-10 px-10">
+      <Link to={router === "/" ? "/history" : "/"}>
+        <div className="text-2xl font-bold cursor-pointer">
+          {router === "/" ? "HISTORY" : "HOME"}
         </div>
-      </div>
-      <Link to="/history">
-        <div className="text-2xl font-bold cursor-pointer">HISTORY</div>
       </Link>
     </div>
   );
--- client/src/pages/History.tsx
+++ client/src/pages/History.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Header from "../components/History/Header";
+import Links from "../components/History/Links";
 
 const History = () => {
   return (
     <div>
       <Navbar />
+      <Header />
+      <Links />
     </div>
   );
 };
